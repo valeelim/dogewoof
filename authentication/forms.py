af6058beb909a5b107dfd3e6c6a1ff6ac2a429b4
Valee <valeriansalim@gmail.com>
2022-10-15 22:44:40 +0700

    created login and register user

---
+++ authentication/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'required': '',
+            'name': 'username',
+            'id': 'username',
+            'type': 'text',
+            'placeholder': 'Uncle Roger',
+            'class': 'form-control',
+            'value': '',
+        })
+        self.fields['password1'].widget.attrs.update({
+            'required': '',
+            'name': 'password1',
+            'id': 'password1',
+            'type': 'password',
+            'placeholder': 'Enter Password',
+            'class': 'form-control',
+        })
+        self.fields['password2'].widget.attrs.update({
+            'required': '',
+            'name': 'password2',
+            'id': 'password2',
+            'type': 'password',
+            'placeholder': 'Re-type Password',
+            'class': 'form-control',
+        })
+        self.fields['email'].widget.attrs.update({
+            'required': '',
+            'name': 'email',
+            'id': 'email',
+            'type': 'email',
+            'placeholder': 'uncle@roger.com',
+            'class': 'form-control',
+        })
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2',)
+
+    def clean(self):
+        super(RegisterForm, self).clean()
+        username = self.cleaned_data.get('username')
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        
+        if password1 != password2:
+            self._errors['password2'] = self.error_class(['Password mismatch'])
+        if len(password1) < 8:
+            self._errors['password1'] = self.error_class(['Password length must be at least 8 characters'])
+        if len(username) < 4:
+            self._errors['username'] = self.error_class(['Username must be at least 4 characters'])
+
+        return self.cleaned_data
---
+++ authentication/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from .forms import RegisterForm
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse('home:index'))
+            response.set_cookie('username', username, max_age=60*60*24*14)
+            return response
+        else:
+            return render(request, 'login.html', {'notFound': True})
+    return render(request, 'login.html', {})
+
+def register_user(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('authentication:login')
+        else:
+            return render(request, 'register.html', {'form': form})
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('authentication:login'))
+    response.delete_cookie('username')
+    return response
