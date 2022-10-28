8ae05425f0cd8fd09518cf5c6bc5e7a5a924be65
Valee <76864467+valeelim@users.noreply.github.com>
2022-10-28 23:19:46 +0700

    Revert "Homepage"

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,4 @@ app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact-us/', views.contactUs, name='contact_us'),
-    path('make-donation/', views.makeDonation, name='make_donation'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse
-from django.db.models import Sum
-from .models import ContactUs, Donation
-from .forms import DonationForm
+from .models import ContactUs
 
 def index(request):
-    form = DonationForm()
-    donationSum = Donation.objects.aggregate(Sum('amount'))
-    return render(request, 'index.html', {
-        'total_donation': 0 if donationSum['amount__sum'] == None else donationSum['amount__sum'],
-        'form': form
-    })
+    return render(request, 'index.html', {})
 
 @login_required
 def contactUs(request):
@@ -25,20 +17,3 @@ def contactUs(request):
         newContactUs.save()
         return JsonResponse({'message': 'success'})
     return render(request, 'index.html', {})
-
-@login_required
-def makeDonation(request):
-    if request.method == 'POST':
-        form = DonationForm(request.POST)
-        if form.is_valid():
-            amount = form.cleaned_data['amount']
-            newDonation = Donation(
-                user=request.user,
-                amount=amount
-            )
-            newDonation.save()
-            return JsonResponse({
-                'donated_amount': amount
-            })
-    return HttpResponseRedirect(reverse('home:index'))
-            
