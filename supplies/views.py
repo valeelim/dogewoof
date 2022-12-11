e2fe0035bfc0a937cd5daf5419051707063b5221
Ayu Putri Dewi Fitriyani <ayuputridf2@.com>
2022-12-11 21:17:26 +0700

    views

--- supplies/views.py
+++ supplies/views.py
@@ -25,7 +25,7 @@ def show_supplies(request):
     return render(request, 'supplies.html', context)
     
 def show_json(request):
-    data = Product.objects.filter(user=request.user).all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
  
 def add_product(request):
