a20b90e94b9074c81ea46934b16ed77d38d3292f
Ernest Benedictus Manurung <ernestbenedictusmanurung@beem.local>
2022-12-12 16:47:20 +0700

    addJSON

--- faq/urls.py
+++ faq/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from faq.views import mainpage, input_question
+from faq.views import mainpage, input_question, json_forum
 
 
 
@@ -8,4 +8,5 @@ app_name = 'faq'
 urlpatterns = [
     path('', mainpage, name='mainpage'),
     path('input_question/', input_question, name='input_question'),
+    path('json/', json_forum, name='json_forum'),
 ]
